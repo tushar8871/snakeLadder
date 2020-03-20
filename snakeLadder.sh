25afa8256c81903651be9d7b609610b861806f63
tushar8871 <tusharkhachane6@gmail.com>
2020-03-20 19:41:42 +0530

    Added :Toss to check if snake comes or play comes or no play

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -3,9 +3,27 @@
 #constants
 STARTPOSITION=0;
 ENDPOSITION=100;
+SNAKE=0;
+NOPLAY=1;
+PLAY=2;
 
 #variables
 currentPosition=0;
 
 #start execution here
-randomPosition=$(($((RANDOM%6))+1))
+
+rollToPlay=$((RANDOM%3))
+
+case $rollToPlay in
+	$SNAKE)
+		randomPosition=$(($((RANDOM%6))+1))
+		currentPosition=$((currentPosition-randomPosition))
+		;;
+	$PLAY)
+		randomPosition=$(($((RANDOM%6))+1))
+		currentPosition=$((currentPosition+randomPosition))
+		;;
+	$NOPLAY)
+		currentPosition=$((currentPosition+0))
+		;;
+esac
