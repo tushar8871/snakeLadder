cad11d4f30f04bf8af2733e66a1089b568f41fe8
tushar8871 <tusharkhachane6@gmail.com>
2020-03-21 10:10:47 +0530

    Added : repeat game until player reach 100

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -12,18 +12,25 @@ currentPosition=0;
 
 #start execution here
 
-rollToPlay=$((RANDOM%3))
-
-case $rollToPlay in
-	$SNAKE)
-		randomPosition=$(($((RANDOM%6))+1))
-		currentPosition=$((currentPosition-randomPosition))
-		;;
-	$PLAY)
-		randomPosition=$(($((RANDOM%6))+1))
-		currentPosition=$((currentPosition+randomPosition))
-		;;
-	$NOPLAY)
-		currentPosition=$((currentPosition+0))
-		;;
-esac
+while [[ $currentPosition -ne $ENDPOSITION ]]
+do
+	rollToPlay=$((RANDOM%3))
+	case $rollToPlay in
+		$SNAKE)
+			randomPosition=$(($((RANDOM%6))+1))
+			if [[ $currentPosition -eq 0 || $currentPosition -lt $randomPosition ]]
+			then
+				currentPosition=$currentPosition;
+			else
+				currentPosition=$((currentPosition-randomPosition))
+			fi
+			;;
+		$PLAY)
+			randomPosition=$(($((RANDOM%6))+1))
+			currentPosition=$((currentPosition+randomPosition))
+			;;
+		$NOPLAY)
+			currentPosition=$((currentPosition+0))
+			;;
+	esac
+done
